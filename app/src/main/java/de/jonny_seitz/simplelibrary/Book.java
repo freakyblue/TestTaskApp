80fffd0248ffddac334f6f64413bd3f9d7fb838d
jonny <github@jonny-seitz.de>
2018-01-15 18:59:38 +0700

    Constructor input tests

--- app/src/main/java/de/jonny_seitz/simplelibrary/Book.java
+++ app/src/main/java/de/jonny_seitz/simplelibrary/Book.java
@@ -12,6 +12,20 @@ public class Book {
     private String description;
 
     public Book(String title, String author, String genre, String description) {
+        if (title == null)
+            throw new NullPointerException("title can't be null!");
+        if (title.equals(""))
+            throw new IllegalArgumentException("title can't be empty!");
+        if (author == null)
+            throw new NullPointerException("author can't be null!");
+        if (author.equals(""))
+            throw new IllegalArgumentException("author can't be empty!");
+        if (genre == null)
+            throw new NullPointerException("genre can't be null!");
+        if (genre.equals(""))
+            throw new IllegalArgumentException("genre can't be empty!");
+        if (description == null)
+            throw new NullPointerException("description can't be null!");
         this.title = title;
         this.author = author;
         this.genre = genre;
