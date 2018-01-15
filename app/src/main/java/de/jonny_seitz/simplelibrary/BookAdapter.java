8e05bb9f8f1d44828d089bb30c655ea83546869e
jonny <github@jonny-seitz.de>
2018-01-15 20:14:30 +0700

    Add BookAdapter

---
+++ app/src/main/java/de/jonny_seitz/simplelibrary/BookAdapter.java
@@ -0,0 +1,28 @@
+package de.jonny_seitz.simplelibrary;
+
+import android.app.Activity;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+
+import java.util.List;
+
+/**
+ * Created by Jonny on 15/01/18.
+ */
+
+public class BookAdapter extends ArrayAdapter {
+
+    public BookAdapter(Activity context, List<Book> books) {
+        super(context, 0, books);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        return super.getView(position, convertView, parent);
+    }
+    
+}
