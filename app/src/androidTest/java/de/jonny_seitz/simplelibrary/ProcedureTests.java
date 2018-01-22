f4381d01bf1603eb00c71d586f9e807d16b4ca15
jonny <github@jonny-seitz.de>
2018-01-22 21:02:39 +0700

    Add nameLibraryTest

--- app/src/androidTest/java/de/jonny_seitz/simplelibrary/ProcedureTests.java
+++ app/src/androidTest/java/de/jonny_seitz/simplelibrary/ProcedureTests.java
@@ -1,26 +1,36 @@
 package de.jonny_seitz.simplelibrary;
 
+import android.support.test.InstrumentationRegistry;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
+import android.support.v7.widget.Toolbar;
+import android.widget.TextView;
 
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu;
+import static android.support.test.espresso.action.ViewActions.clearText;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isAssignableFrom;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.supportsInputMethods;
+import static android.support.test.espresso.matcher.ViewMatchers.withContentDescription;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withParent;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.CoreMatchers.allOf;
 
 /**
  * Created by Jonny on 17/01/18.
  */
 
 @RunWith(AndroidJUnit4.class)
-public class AddBookTest {
+public class ProcedureTests {
 
     @Rule
     public ActivityTestRule<MainActivity> activityTestRule =
@@ -40,6 +50,22 @@ public class AddBookTest {
         onView(withId(R.id.button_add)).perform(click());
         onView(withText("Test book")).perform(click());
         Thread.sleep(1000);
+    }
+
+    @Test
+    public void nameLibraryTest() throws Exception {
+        Thread.sleep(500);
+        openActionBarOverflowOrOptionsMenu(InstrumentationRegistry.getTargetContext());
+        onView(withText("Settings")).perform(click());
+        onView(withText("Library name")).perform(click());
+        onView(supportsInputMethods())
+                .perform(clearText())
+                .perform(typeText("Congress Library"));
+        onView(withText("OK")).perform(click());
+        onView(withContentDescription(R.string.abc_action_bar_up_description)).perform(click());
+        onView(allOf(isAssignableFrom(TextView.class), withParent(isAssignableFrom(Toolbar.class))
+        )).check(matches(withText("Congress Library")));
+        Thread.sleep(3000);
     }
 
 }
